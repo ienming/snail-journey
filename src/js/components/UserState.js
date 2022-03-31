1551801345b75f2d0ed16bdaf383ce6abffcf78c
Lucien Hsieh <mlhsieh@Luciende-MacBook-Pro-2.local>
2022-03-31 11:54:51 +0800

    add trashes and guys data in UserState component

--- src/js/components/UserState.js
+++ src/js/components/UserState.js
@@ -7,10 +7,13 @@ const UserState = {
             <li v-for="(el, idx) of achievements"><span>{{idx}}: </span>{{el}}</li>
             <span>{{userAchieved}}</span>
         </ul>
-        
+        <h4>今天的垃圾：{{dailyTrashes.length}}</h4>
+        <h4>已經撿起：{{userGotTrashes.length}}</h4>
+        <h4>今天的好壞人：{{dailyGuys.length}}</h4>
+        <h4>已經評價：{{userJudges.length}}</h4>
     </div>
     `,
-    props: ['achievements', 'userAchieved'],
+    props: ['achievements', 'userAchieved', 'dailyTrashes', 'dailyGuys', 'userGotTrashes', 'userJudges'],
     data(){
         return {}
     }
