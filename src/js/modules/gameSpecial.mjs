8d70ae38a898ca4e595d6172166afd3f0a07c080
Lucien Hsieh <mlhsieh@Luciende-MacBook-Pro-3.local>
2022-05-04 12:15:16 +0800

    update 特殊蝸牛

--- src/js/modules/gameSpecial.mjs
+++ src/js/modules/gameSpecial.mjs
@@ -6,49 +6,52 @@ specialContainer.scale.set(scale)
 
 let poses = [
     {
-        x: -1300,
-        y: -500
+        x: 1800,
+        y: 700
     }, {
         x: 1000,
         y: 800
     }, {
         x: 1500,
         y: 1000
+    }, {
+        x: 0,
+        y: 1050
     }
 ]
 let posId = 0, sp, spIsClear = false
 function generateSpecial(){
     // 判斷每日更新
-    let nowTime = new Date().getMinutes()
-    let recordGotSpecial = vm.$data.userRecord.gotSpecial
-    if (!recordGotSpecial && nowTime%2 == 0){
-        drawSpecial()
-        updatePos()
-    }else{
-        console.log("今天已經抓到特殊蝸牛了")
-    }
-    window.setInterval(()=>{
-        if (new Date().getMinutes() !== nowTime && new Date().getMinutes()%2 == 0){
-            nowTime = new Date().getMinutes()
-            console.log("新日子，重新產生特殊蝸牛")
-            vm.$data.user.gotSpecial = false
-            while(specialContainer.children.length > 0){
-                specialContainer.removeChild(specialContainer.children[0])
-            }
-            drawSpecial()
-            updatePos()
-            vm.$data.user.gotSpecial = false
-        }
-    }, 1000)
+    // let nowTime = new Date().getMinutes()
+    // let recordGotSpecial = vm.$data.userRecord.gotSpecial
+    // if (!recordGotSpecial && nowTime%2 == 0){
+    //     drawSpecial()
+    //     updatePos()
+    // }else{
+    //     console.log("今天已經抓到特殊蝸牛了")
+    // }
+    // window.setInterval(()=>{
+    //     if (new Date().getMinutes() !== nowTime && new Date().getMinutes()%2 == 0){
+    //         nowTime = new Date().getMinutes()
+    //         console.log("新日子，重新產生特殊蝸牛")
+    //         vm.$data.user.gotSpecial = false
+    //         while(specialContainer.children.length > 0){
+    //             specialContainer.removeChild(specialContainer.children[0])
+    //         }
+    //         drawSpecial()
+    //         updatePos()
+    //         vm.$data.user.gotSpecial = false
+    //     }
+    // }, 1000)
+    drawSpecial()
+    updatePos()
 }
 
 function updatePos(){
     let timer  = window.setInterval(()=>{
         if (spIsClear == false){
             posId = getRandom(0, poses.length-1)
-            sp.x = poses[posId].x
-            sp.y = poses[posId].y
-            fastMove(sp)
+            fastMove(posId)
         }else {
             clearInterval(timer)
         }
@@ -62,6 +65,14 @@ function drawSpecial(){
     sp.anchor.set(0.5)
     sp.interactive = true
     sp.buttonMode = true
+    gsap.to(sp, .3, { //抖動
+        pixi: {
+            scaleX: 1.1,
+            scaleY: 0.9
+        },
+        yoyo: true,
+        repeat: -1
+    })
     sp.on("pointerdown", (el)=>{
         vm.$data.user.gotSpecial = true
         el.target.destroy()
@@ -88,21 +99,12 @@ function drawSpecial(){
     mapContainer.addChild(specialContainer)
 }
 
-function fastMove(item){
-    gsap.to(item, 1, {
+function fastMove(posId){
+    gsap.to(sp, .5, {
         pixi: {
-            x: item.x - getRandom(20, 40),
-        },
-        yoyo: true,
-        repeat: -1
-    })
-    gsap.to(item, 1, {
-        pixi: {
-            scaleX: 1.08,
-            scaleY: 0.96
-        },
-        yoyo: true,
-        repeat: -1
+            x: poses[posId].x,
+            y: poses[posId].y,
+        }
     })
 }
 
