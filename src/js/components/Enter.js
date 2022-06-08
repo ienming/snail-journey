2faa35f85df146e6e2d08f02cf054d87247dc26c
Lucien Hsieh <mlhsieh@Luciende-MacBook-Pro-3.local>
2022-06-08 23:27:17 +0800

    add instruction

--- src/js/components/Enter.js
+++ src/js/components/Enter.js
@@ -8,6 +8,7 @@ const Enter = {
                         <transition name="fade">
                             <div class="t-a-c" v-if="!loading">
                                 <p class="t-c-g t-z-1 mt-1">請注意音量</p>
+                                <p class="t-c-g t-z-1 mt-1">若無法進入，請關閉廣告攔截後重新整理</p>
                                 <p class="t-c-g t-z-1" style="padding: 8px 16px; background-color: white; border-radius: 50px;">建議使用電腦或筆電開始體驗</p>
                                 <button class="mt-2 my-btn w-100" @click="startGame">
                                     進入綠洲
